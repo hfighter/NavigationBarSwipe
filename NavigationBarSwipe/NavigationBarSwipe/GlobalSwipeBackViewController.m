77a4774da2f9289e3cc34b29c28f487aa8dd5717
honghui <hong06xu12@163.com>
2017-03-16 16:33:20 +0800

    自定义返回，支持侧边返回；全屏返回

---
+++ NavigationBarSwipe/NavigationBarSwipe/GlobalSwipeBackViewController.m
@@ -0,0 +1,37 @@
+//
+//  GlobalSwipeBackViewController.m
+//  NavigationBarSwipe
+//
+//  Created by Boxfish on 17/3/16.
+//  Copyright © 2017年 燊燊科技. All rights reserved.
+//
+
+#import "GlobalSwipeBackViewController.h"
+
+@interface GlobalSwipeBackViewController ()
+
+@end
+
+@implementation GlobalSwipeBackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
